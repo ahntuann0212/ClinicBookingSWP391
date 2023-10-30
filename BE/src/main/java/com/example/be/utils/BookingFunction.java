c19be6fc852e36cccb1e80f3dbfd7ced100d6d70
ahntuann0212 <ahntuann@gmail.com>
2023-10-30 23:47:56 +0700

    ul

---
+++ BE/src/main/java/com/example/be/utils/BookingFunction.java
@@ -0,0 +1,11 @@
+package com.example.be.utils;
+
+
+
+import com.example.be.entities.Booking;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookingFunction {
+
+}
---
+++ BE/src/main/java/com/example/be/utils/UserResponceClinicFunction.java
@@ -0,0 +1,13 @@
+package com.example.be.utils;
+
+
+import com.example.be.entities.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class UserResponceClinicFunction {
+	
+
+}
