af8df9a4da47bc9e99a1cb84c9073f57005dec87
SWP <hunglmhe153198@fpt.edu.vn>
2023-11-09 21:27:45 +0700

    Entities

---
+++ BE/src/main/java/com/example/be/entities/ExpertCode.java
@@ -0,0 +1,30 @@
+package com.example.be.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name ="expert_code")
+public class ExpertCode extends AuditEntity implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	public ExpertCode() {
+		
+	}
+	
+	@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ExpertCode expertCode = (ExpertCode) o;
+        return Objects.equals(this.getId(), expertCode.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getId());
+    }
+}
