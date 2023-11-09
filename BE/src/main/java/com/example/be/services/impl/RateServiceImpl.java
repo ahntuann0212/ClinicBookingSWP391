fa461ce7d64d67f80ada4ce17130022f80fb5f8f
SWP <hunglmhe153198@fpt.edu.vn>
2023-11-09 21:24:56 +0700

    Service

---
+++ BE/src/main/java/com/example/be/services/impl/RateServiceImpl.java
@@ -0,0 +1,94 @@
+package com.example.be.services.impl;
+
+import com.example.be.dto.ClinicsRequest;
+import com.example.be.dto.RateRequest;
+import com.example.be.entities.Clinic;
+import com.example.be.entities.Rate;
+import com.example.be.entities.User;
+import com.example.be.payload.Data;
+import com.example.be.payload.DataResponse;
+import com.example.be.repository.*;
+import com.example.be.security.UserPrincipal;
+import com.example.be.services.RateService;
+import com.example.be.utils.Constant;
+import com.example.be.utils.RateFunction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class RateServiceImpl implements RateService{
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	ClinicRepository clinicRepository;
+	
+	@Autowired
+	ClinicServiceImpl clinicServiceImpl;
+	@Autowired
+	CommentRepositiry commentRepositiry;
+	
+	@Autowired
+	BookingRepository bookingRepository;
+	
+	@Autowired
+	RateRepository rateRepository;
+	
+	@Override
+	public DataResponse addRate(UserPrincipal currentUser, RateRequest rateRequest) {
+		
+		Rate checkRate = rateRepository.checkUserAndDoctorForRate(currentUser.getId(), rateRequest.getExpert().getId());
+		User user = userRepository.getOne(currentUser.getId());
+		
+		if(checkRate !=null) {
+			rateRepository.delete(checkRate);
+			if(user != null) {
+				Rate rate = new Rate();
+				rate.setUser(user);
+				
+				rate.setNumberStar(rateRequest.getNumberStar());
+				
+				User expert = userRepository.getOne(rateRequest.getExpert().getId());
+				rate.setExpert(expert);
+				
+				Clinic clinic = clinicRepository.getOne(rateRequest.getClinic().getId());
+				rate.setClinic(clinic);
+				
+				Rate ratedata =rateRepository.save(rate);
+				
+				return new DataResponse(true, new Data(Constant.ADD_RATE_SUCCESS,HttpStatus.OK.value(),ratedata));
+			}
+		}else {
+			if(user != null) {
+				Rate rate = new Rate();
+				rate.setUser(user);
+				
+				rate.setNumberStar(rateRequest.getNumberStar());
+				
+				User expert = userRepository.getOne(rateRequest.getExpert().getId());
+				rate.setExpert(expert);
+				
+				Clinic clinic = clinicRepository.getOne(rateRequest.getClinic().getId());
+				rate.setClinic(clinic);
+				
+				Rate ratedata =rateRepository.save(rate);
+					
+				return new DataResponse(true, new Data(Constant.ADD_RATE_SUCCESS,HttpStatus.OK.value(),ratedata));
+			}
+		}
+		
+		return new DataResponse(false, new Data(Constant.ADD_RATE_UNSUCCESS,HttpStatus.BAD_REQUEST.value()));
+		
+	}
+	
+	@Override
+	public DataResponse getRatetDoctor(ClinicsRequest clinicsRequest) {
+		Set<Rate> rateExperts = rateRepository.getRatesByIdClincAndIdExpert(clinicsRequest.getIdClinic(),clinicsRequest.getIdDoctor());
+		Double countRate = RateFunction.getRateDoctor(rateExperts);
+		return new DataResponse(true, new Data("get rate success !",HttpStatus.OK.value(),countRate));
+	}
+}
