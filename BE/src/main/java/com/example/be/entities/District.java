c0c8ebf6c767d8c546f86d8c3985dfac9687ec41
SWP <hunglmhe153198@fpt.edu.vn>
2023-11-10 13:37:06 +0700

    Entiti

--- BE/src/main/java/com/example/be/entities/District.java
+++ BE/src/main/java/com/example/be/entities/District.java
@@ -1,10 +1,9 @@
 package com.example.be.entities;
 
-import java.io.Serializable;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "district")
